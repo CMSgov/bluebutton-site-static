b9e80fc3d555fc841abcc5703ceab4783b6a72c1
Ryan Nagle <rmnagle@gmail.com>
2018-02-09 11:44:56 -0600

    first pass at tf configs for test site

---
+++ ops/terraform/test.bluebutton.cms.gov/main.tf
@@ -0,0 +1,34 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+##
+# Data providers
+##
+data "template_file" "bucket_policy" {
+  template = "${file("${path.module}/templates/bucket-policy.json")}"
+
+  vars {
+    bucket_name = "${var.bucket_name}"
+    allowed_ips = "${join("\", \"", var.allowed_ips)}"
+  }
+}
+
+##
+# S3 Bucket
+##
+resource "aws_s3_bucket" "main" {
+  bucket = "${var.bucket_name}"
+  acl    = "public-read"
+  policy = "${data.template_file.bucket_policy.rendered}"
+
+  website {
+    index_document = "index.html"
+    error_document = "index.html"
+  }
+
+  tags {
+    Name        = "${var.bucket_name}"
+    environment = "${var.env}"
+  }
+}
---
+++ ops/terraform/test.bluebutton.cms.gov/variables.tf
@@ -0,0 +1,11 @@
+variable "app" { }
+
+variable "stack" { }
+
+variable "env" { }
+
+variable "bucket_name" { }
+
+variable "allowed_ips" {
+  type = "list"
+}
