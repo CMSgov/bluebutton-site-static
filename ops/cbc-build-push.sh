b2221710a6c9f6578834779993be1f14f2d3bb4d
Don Seymour <don.seymour@adhocteam.us>
2022-04-19 13:07:35 -0500

    bump ruby and bundler versions on build image

--- ops/cbc-build-push.sh
+++ ops/cbc-build-push.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-DOCKER_TAG=${1:-rb25}
+DOCKER_TAG=${1:-rb26}
 
 docker build --file Dockerfile.cbc-build \
-  --build-arg RUBY_VERSION=${2:-2.5} \
+  --build-arg RUBY_VERSION=${2:-2.6} \
   --tag public.ecr.aws/f5g8o1y9/bb2-cbc-build:${DOCKER_TAG} \
   .
 
