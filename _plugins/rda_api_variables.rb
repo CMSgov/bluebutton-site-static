f10668679a91331b827744224576b7aa61150641
Brian Burton <brian.burton@burton-computer.com>
2022-09-20 14:51:12 -0400

    PACA-733: RDA API data dictionary plugin. (#270)
    * PACA-733: Data, layout, and plugin to render RDA API data dictionary variables.
    
    * Adds new plugin to README file.
    
    * Improves language in README based on review feedback.

---
+++ _plugins/rda_api_variables.rb
@@ -0,0 +1,113 @@
+# This file provides a VariableGenerator plugin for Jekyll, which can
+# produce additional pages in the site's rendered output. See the documentation
+# on the class below for more details.
+
+module RdaApi
+  # Each Variable instance represents a field derived from the RDA API data
+  # dictionary CSV extract file located in the _data directory.
+  #
+  # Data elements are extracted from the CSV file using the column headers to
+  # identify each field.  Only the fields required to render a page are extracted
+  # from the CSV records.
+  class Variable
+    
+    attr_reader :id, :source_system
+
+    def initialize(csv_row)
+      # Extract just the fields we need to display.
+      @data = parse_csv_row(csv_row)
+
+      # Pull out the `id` attribute that uniquely identifies this variable.
+      @id = @data['id']
+
+      # Pull out the source system (FISS or MCS) from which this variable originates.
+      @source_system = @data['source_system'].downcase
+    end
+
+    # Takes the attributes of @data and merges them into another hash.
+    def export_data(target)
+      target.merge!(@data)
+    end
+
+    # Extract the columns we need into a new Hash.
+    def parse_csv_row(csv_row)
+      data = {}
+
+      data['source_system'] = csv_row['Source system']
+      data['field'] = csv_row['Field name']
+      data['source_field'] = csv_row['Source copybook field label']
+      data['alt_source_field'] = csv_row['Copybook data dictionary name (if different)']
+      data['source_definition'] = split(csv_row['Source system definition'])
+      data['version'] = csv_row['Version added']
+
+      # We prefer the copybook data dictionary name if there is one.
+      if data['alt_source_field'] =~ %r{^[A-Z]+(-[A-Z]+)*$}
+        data['field_label'] = data['alt_source_field']
+      else
+        data['field_label'] = data['source_field']
+      end
+
+      # We force lowercase here to get prettier URLs.
+      data['id'] = data['field_label'].downcase
+
+      return data
+    end
+
+    def split(text)
+      if text.nil? || text.empty?
+        []
+      else
+        text.split("\n").reject {|s| s.empty?}
+      end
+    end
+
+  end
+end
+
+module Jekyll
+  # Each VariablePage instance represents a Jekyll page that can be rendered,
+  # providing documentation for the BlueButtonApi::Variable that it represents.
+  class VariablePage < Page
+    def initialize(site, layout, variable)
+      @site = site
+      @base = site.source
+      @dir = File.join('resources', 'variables', variable.source_system, variable.id)
+      @name = 'index.html'
+
+      self.process(@name)
+
+      # Despite the name, the most important thing that this method does is to
+      # set the specified file as the layout to use for this page.
+      self.read_yaml(File.join(@base, '_layouts'), layout)
+
+      # Values in the page's `data` hash can be used by the layout for rendering.
+      variable.export_data(self.data)
+    end
+  end
+
+  # VariableGenerator is a Jekyll Generator plugin, which can produce pages that
+  # will be included in the site's rendered output.
+  class VariableGenerator < Generator
+    safe true
+
+    def generate(site)
+      Jekyll.logger.debug('VariableGenerator:', 'Verifying configuration...')
+      return unless site.config.key? 'rda_api'
+      config = site.config['rda_api']
+      
+      return unless config.key? 'layout'
+      layout = config['layout']
+
+      csv_file_key = config['csv_file_key']
+      return unless config.key? 'csv_file_key'
+      Jekyll.logger.debug('VariableGenerator:', 'Configured.')
+
+      # Parse each XML file and generate a `VariablePage` for each `<variable/>`
+      # entry.
+      site.data[csv_file_key].each do |csv_row|
+        variable_obj = RdaApi::Variable.new(csv_row)
+	      site.pages << VariablePage.new(site, layout, variable_obj)
+      end
+    end
+  end
+end
