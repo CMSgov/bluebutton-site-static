b5de15ae8301ddd20f301d5defef1b6afc92f265
Don Seymour <don.seymour@adhocteam.us>
2022-12-21 15:25:28 -0600

    Bb2 1937 upgrade build contianer to ruby 3.1.2 (#286)
    * update ruby build image
    
    * update v2 terms path with trailing slash

--- ops/cbc-build-push.sh
+++ ops/cbc-build-push.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-DOCKER_TAG=${1:-rb26}
+DOCKER_TAG=${1:-rb312}
 
 docker build --file Dockerfile.cbc-build \
-  --build-arg RUBY_VERSION=${2:-2.6} \
+  --build-arg RUBY_VERSION=${2:-3.1.2} \
   --tag public.ecr.aws/f5g8o1y9/bb2-cbc-build:${DOCKER_TAG} \
   .
 
