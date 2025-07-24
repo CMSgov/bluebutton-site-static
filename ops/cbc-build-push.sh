223c2219c1bf78730f9bf37ac9ac848e1e436a1e
Noor <noorulla@gmail.com>
2025-07-24 15:32:39 -0500

    Update to ECR public new url

--- ops/cbc-build-push.sh
+++ ops/cbc-build-push.sh
@@ -4,7 +4,7 @@ DOCKER_TAG=${1:-rb312}
 
 docker build --file Dockerfile.cbc-build \
   --build-arg RUBY_VERSION=${2:-3.3.5} \
-  --tag public.ecr.aws/f5g8o1y9/bb2-cbc-build:${DOCKER_TAG} \
+  --tag public.ecr.aws/q8j7a4l4/bb2-cbc-build:${DOCKER_TAG} \
   .
 
-docker push public.ecr.aws/f5g8o1y9/bb2-cbc-build:${DOCKER_TAG}
+docker push public.ecr.aws/q8j7a4l4/bb2-cbc-build:${DOCKER_TAG}
