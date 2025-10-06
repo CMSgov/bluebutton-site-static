54ad5583326a6a452b2d6bafeb3472816785ea46
Jonathan Hutchison <jonathanhutchison@navapbc.com>
2025-10-06 13:51:55 -0400

    Update page layout structure

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,23 +1,24 @@
+import mdx from '@astrojs/mdx'
+import sitemap from '@astrojs/sitemap'
+import typesafeRoutes from 'astro-typesafe-routes'
 // @ts-check
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-
-import sitemap from '@astrojs/sitemap';
-
-import typesafeRoutes from 'astro-typesafe-routes';
+import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://bluebutton.cms.gov',
+  prefetch: {
+    prefetchAll: true,
+  },
 
   vite: {
     css: {
       preprocessorOptions: {
         scss: {
-          loadPaths: ["node_modules/@uswds/uswds/packages"],
+          loadPaths: ['node_modules/@uswds/uswds/packages'],
           quietDeps: true,
-        }
-      }
+        },
+      },
     },
   },
 
