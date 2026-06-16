76980daa66811084a66dca05b0c6c9af11d6c1b7
Jonathan Hutchison <jonathanhutchison@navapbc.com>
2026-06-16 16:46:29 -0400

    Link tidying

--- src/utils/trailing-slash.ts
+++ src/utils/trailing-slash.ts
@@ -2,9 +2,13 @@ import { trailingSlash } from 'astro:config/server'
 
 // Normalize an internal path's trailing slash to match the global Astro 'trailingSlash' setting
 export function withTrailingSlash(path: string): string {
-  if (trailingSlash === 'always')
-    return path.endsWith('/') ? path : `${path}/`
-  if (trailingSlash === 'never')
+  if (trailingSlash === 'always') {
+    return path.endsWith('/')
+      ? path.replace(/\/+$/g, '/') // Ensure only one trailing slash
+      : `${path}/`
+  }
+  if (trailingSlash === 'never') {
     return path.replace(/\/+$/g, '') || '/'
+  }
   return path
 }
