9ddefbff4d5779b47b3a10c93219203de4ac0edb
Jonathan Hutchison <jonathanhutchison@navapbc.com>
2026-06-17 10:53:23 -0400

    Combine internal link handling functions to better handle search and hash params

--- src/utils/internal-to-relative-url.ts
+++
@@ -1,7 +0,0 @@
-import { SITE_METADATA } from './constants'
-
-export function internalToRelativeUrl(url: string) {
-  return url.startsWith(SITE_METADATA.url)
-    ? url.replace(SITE_METADATA.url, '')
-    : url
-}
